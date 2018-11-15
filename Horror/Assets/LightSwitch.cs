5c0fff696138e8421329787fbd5ebb2612d17c7d
jedineeson <1810027@bart.ca>
2018-11-14 20:34:16 -0500

    yoyoyo

---
+++ Horror/Assets/LightSwitch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_LightOnObjs;
+
+    [SerializeField]
+    private GameObject m_LightOffObjs;
+
+    [SerializeField]
+    private bool m_LightIsOn = true;
+
+    private void Start()
+    {
+        SetSceneObjs();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            m_LightIsOn = !m_LightIsOn;
+            SetSceneObjs();
+        }
+    }
+
+    private void SetSceneObjs()
+    {
+        if(m_LightIsOn)
+        {
+            m_LightOnObjs.SetActive(true);
+            m_LightOffObjs.SetActive(false);
+        }
+        else
+        {
+            m_LightOnObjs.SetActive(false);
+            m_LightOffObjs.SetActive(true);
+        }
+    }
+}
